e1b6b8b916810648ac2723d7e51d3dce3133e268
Marquise Piton <pitonmarquise@gmail.com>
2021-04-19 14:30:59 -0400

    Added a new component and css file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
+  
+
   return (
     <div className="expense-item">
-      <div>
-        <div>props.date.toLocaleString('en-US')</div>
-        <div>Year</div>
-        <div>Day</div>
-      </div>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
