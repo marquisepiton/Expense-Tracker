c0e68cfcf268ffc21d2a673d04f2737610fec51a
Marquise Piton <pitonmarquise@gmail.com>
2021-04-20 11:59:14 -0400

    Added 3 new component files and css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import  Expenses from './components/Expenses';
-
+import  Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,10 +2,8 @@ import './Card.css';
 
 
 function Card(props) {
+  
   const classes = 'card ' + props.className;
-  
-
-  
   return <div className={classes}>{props.children}</div>;
 }
 
